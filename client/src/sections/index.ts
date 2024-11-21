bf9aedd899fdfaed7f510b60d1d212a8aeb1a7c0
Mitul Patel <me@mitulpa.tel>
2024-11-21 13:37:44 -0500

    feat(app): tinyhouse application

---
+++ client/src/sections/index.ts
@@ -0,0 +1,8 @@
+export { AppHeader } from "./AppHeader";
+export { Home } from "./Home";
+export { Host } from "./Host";
+export { Listing } from "./Listing";
+export { Listings } from "./Listings";
+export { Login } from "./Login";
+export { NotFound } from "./NotFound";
+export { User } from "./User";
